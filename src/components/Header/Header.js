d9b403a46dee17882e1d5a3b616c29c571179edc
Nmcleon <nmcleon10@gmail.com>
2024-06-05 16:09:06 +0200

    create header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,54 @@
 import React from 'react';
 import './Header.css';
 import logo from '../assets/logo.svg';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="header-container">
+        <header className="header">
+          <div className="header-logo">
+            <img
+              src={logo}
+              className="logo"
+              alt="logo"
+              onClick={() => navigate('/')}
+            />
+          </div>
+          <div className="header-items">
+            <ul className="header-links">
+              <li
+                className={`header-link ${pathMatchRoute('/') ? 'active' : ''}`}
+                onClick={() => navigate('/')}
+              >
+                Home
+              </li>
+              <li
+                className={`header-link ${
+                  pathMatchRoute('/offers') ? 'active' : ''
+                }`}
+                onClick={() => navigate('/offers')}
+              >
+                Offers
+              </li>
+              <li
+                className={`header-link ${
+                  pathMatchRoute('/sign-in') ? 'active' : ''
+                }`}
+                onClick={() => navigate('/sign-in')}
+              >
+                Sign in
+              </li>
+            </ul>
+          </div>
         </header>
       </div>
     </>
