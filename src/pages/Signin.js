e33f01e786b8e19356a546d090c36d9e34b05d66
Nmcleon <nmcleon10@gmail.com>
2024-06-05 21:28:55 +0200

    create Signin page

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export class Signin extends Component {
-  render() {
-    return <div>Signin</div>;
-  }
+export default function Signin() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <>
+      <section className="auth-container">
+        <h1 className="auth-header">Sign In</h1>
+        <div className="auth">
+          <div className="auth-img">
+            <img
+              src={'./images/auth-key.jpg'}
+              alt="key"
+              className="auth-image"
+            />
+          </div>
+          <div>
+            <form className="auth-form">
+              <input
+                type="email"
+                value={email}
+                id="email"
+                onChange={onChange}
+                placeholder="Email"
+              />
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                id="password"
+                onChange={onChange}
+                placeholder="Password"
+              />
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-
-export default Signin;
