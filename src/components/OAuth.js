2c74d9c66fb402d184daff42edb4111bf8c4225b
Nmcleon <nmcleon10@gmail.com>
2024-06-07 15:57:52 +0200

    add google user Authentification

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { db } from '../firebase';
+import { doc, serverTimestamp, setDoc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function googleAuth() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //console.log(user);
+
+      //check if user exist in db, if not move Google sign in user to users db
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate('/');
+    } catch (error) {
+      toast.error('Google authorization failed');
+      //console.log(error);
+    }
+  }
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button
+      onClick={googleAuth}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
       <FcGoogle className=" text-2xl  bg-white rounded-full mr-2 " />
       Continue with Google
     </button>
