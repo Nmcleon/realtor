b4053985fe718f9c45dfc9fdf9fb8a268d83cd61
Nmcleon <nmcleon10@gmail.com>
2024-06-08 15:31:51 +0200

    create protected private route, profile page and hooks for profile page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Profile from './pages/Profile';
 import Header from './components/Header/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -16,11 +17,13 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/profile" element={<PrivateRoute />}>
+            <Route exact path="/profile" element={<Profile />} />
+          </Route>
           <Route exact path="/sign-in" element={<Signin />} />
           <Route exact path="/sign-up" element={<Signup />} />
           <Route exact path="/forgot-password" element={<ForgotPassword />} />
           <Route exact path="/offers" element={<Offers />} />
-          <Route exact path="/profile" element={<Profile />} />
         </Routes>
       </Router>
       <ToastContainer
