a0879354b767ddcbdce6651b2bfcec5a7509cada
Nmcleon <nmcleon10@gmail.com>
2024-06-16 20:48:21 +0200

    add slider component to home

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -349,7 +349,7 @@ export default function CreateListing() {
             value={description}
             onChange={onChange}
             placeholder="description"
-            maxLength="32"
+            maxLength="200"
             minLength="10"
             required
             className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
